9bc8daedd77fd202fd361035e553028f6fb37f89
Charles Fouquet <fouquetcharles@gmail.com>
2022-12-23 12:41:57 +0100

    Push du 20221223, fin du délai initial

--- js/exo_20221222.js
+++ js/exo_20221222.js
@@ -38,66 +38,109 @@ $.ajax({
         addToCartButtons.each(function(){
             $(this).on("click", function(){
                 if (intoCart == 0) {
-                    $(".cart").empty().append("<div>Voici la liste de vos produits. Vous pouvez changer les quantités si vous le souhaitez.</div><div><span id=\"totalPriceLeft\">Total : <span id=\"totalPrice\"></span>€</span><button class=\"btn btn-primary\" type=\"button\" data-bs-toggle=\"offcanvas\" data-bs-target=\"#offcanvasleft\" aria-controls=\"offcanvasleft\">Valider ma commande</button></div>");
+                    $(".cart").empty().append("<div>Voici la liste de vos produits. Vous pouvez changer les quantités si vous le souhaitez.</div><div id=\"confirmHeader\"><span id=\"totalPriceLeft\">Total : <span id=\"totalPrice\"></span>€</span><button class=\"btn btn-primary confirmOrder\" type=\"button\" data-bs-toggle=\"offcanvas\" data-bs-target=\"#offcanvasleft\" aria-controls=\"offcanvasleft\">Valider ma commande</button></div>");
                 }
-                intoCart++;
-                $("#intoCart").text(intoCart);
                 var productIndex = Number.parseFloat($(this).attr("id").match(regExProduct));
-                $(".cart").append("<div class=\"productInCart\"><div>Désignation : <strong>"+data.produits[productIndex].nom+"</strong></div><button class=\"btn btn-danger\" type=\"button\">Supprimer</button><div><div>Quantité : <input type=\"number\" value=\"1\" class=\"qty\"></div><div>Prix : <strong class=\"unitPrice\">"+data.produits[productIndex].prix+"€</strong></div></div></div>");
+                if (($("#productID-"+productIndex)).length != 0) {
+                    var incrementQty = 1 + Number.parseFloat(($("#productID-"+productIndex)).children("div.inside1").children(".inside2").children(".qty").val());
+                    $("#productID-"+productIndex+" .qty").val(incrementQty);
+                } else {
+                    $(".cart").append("<div class=\"productInCart\" id=\"productID-"+productIndex+"\"><div>Désignation : <strong class=\"prodName\">"+data.produits[productIndex].nom+"</strong></div><button class=\"btn btn-danger deleteButton\" id=\"deleteButton-"+productIndex+"\" type=\"button\">Supprimer</button><div class=\"inside1\"><div class=\"inside2\">Quantité : <input type=\"number\" class=\"qty\"></div><div class=\"inside2\">Prix : <strong class=\"unitPrice\">"+data.produits[productIndex].prix+"€</strong></div></div></div>");
+                    $("#productID-"+productIndex+" .qty").val(1);
+                }
                 var quantitiesInCart = [];
                 $(".qty").each(function(){
                     quantitiesInCart.push(Number.parseFloat($(this).val()));
                 });
-                console.log($(quantitiesInCart));
+                intoCart = 0;
+                quantitiesInCart.forEach(element => {
+                    intoCart += Number.parseFloat(element);
+                });
+                $("#intoCart").text(intoCart);
                 var pricesInCart = []
                 $(".unitPrice").each(function(){
                     pricesInCart.push(Number.parseFloat($(this).html().match(regExProduct)));
                 });
-                console.log($(pricesInCart));
                 if (quantitiesInCart.length == pricesInCart.length) {
                     cartTotal = 0;
                     for (let index = 0; index < quantitiesInCart.length; index++) {
                         cartTotal += quantitiesInCart[index] * pricesInCart[index];
                     }
                 }
-                console.log(cartTotal);
                 $("#totalPrice").text(cartTotal);
+
+                var qtyFields = $(".qty");
+                qtyFields.each(function(){
+                    $(this).keyup(function(){
+                        updateCartTotal();
+                    })
+                    $(this).change(function(){
+                        updateCartTotal();
+                    })
+                });
+
+                var deleteButtons = $(".deleteButton");
+                deleteButtons.each(function(){
+                    $(this).click(function(){
+                        var toDelete = $(this).attr('id').replace("deleteButton-", "productID-");
+                        $("#"+toDelete).remove();
+                        updateCartTotal();
+                        if ($(".cart div").html().match("Votre panier est tristement vide en ce moment") != null) {
+                            intoCart = 0;
+                        }
+                    })
+                });
             });
         });
-
-        // if (($("#addGrade").val() != "") && (isNaN($("#addGrade").val()) == false)) {
-        //     addedGrades.push(parseFloat($("#addGrade").val()));
-        // };
-        // for (let y = 0; y < addedGrades.length; y++) {
-        //     data.etudiants[1].notes.push(Number.parseFloat(addedGrades[y]));
-        // }
-        // if (addedGrades.length > 0) {
-        //     $("#alreadyAdded").text("Notes déjà ajoutées : "+addedGrades.toString().replaceAll(",", ", ")).fadeIn("fast");
-        //     $("#addGrade").val("");
-        // }
-
-        // for (let index = 0; index < data.etudiants.length; index++) {
-        //     var nom_etudiant = data.etudiants[index].nom;
-        //     var prenom_etudiant = data.etudiants[index].prenom;
-        //     var somme = 0;
-        //     var currentStudent = data.etudiants[index];
-        //     for (let index2 = 0; index2 < currentStudent.notes.length; index2++) {
-        //         somme = somme + currentStudent.notes[index2];
-        //     }
-        //     var moyenne = Number.parseFloat(somme / (data.etudiants[index].notes.length)).toFixed(1);
-        //     if (moyenne >= 10) {
-        //         var pass = "Oui";
-        //         var color = "green";
-        //         var back = "bg-green";
-        //     } else {
-        //         var pass = "Non";
-        //         var color = "red";
-        //         var back = "bg-red";
-        //     }
-        //     $("#result").append("<div class =\"card "+back+"\"><p><b>Élève : "+prenom_etudiant+" "+nom_etudiant+"</b></p><p>Moyenne : "+moyenne+" / Admis : <span class=\""+color+"\">"+pass+"</span></p></div>");
-        // }
     },
     error:function(xhr, status){
         alert(xhr.status);
     }
 });
+
+function updateCartTotal() {
+    var cartTotal = 0;
+    var intoCart = 0;
+    var regExProduct = /\d+/g;
+    var quantitiesInCart = [];
+    $(".qty").each(function(){
+        quantitiesInCart.push(Number.parseFloat($(this).val()));
+        intoCart = intoCart + Number.parseFloat($(this).val());
+    });
+    var pricesInCart = []
+    $(".unitPrice").each(function(){
+        pricesInCart.push(Number.parseFloat($(this).html().match(regExProduct)));
+    });
+    if (quantitiesInCart.length == pricesInCart.length) {
+        cartTotal = 0;
+        for (let index = 0; index < quantitiesInCart.length; index++) {
+            cartTotal += quantitiesInCart[index] * pricesInCart[index];
+        }
+    }
+    if (intoCart == 0) {
+        $("#intoCart").text("");
+    } else {
+        $("#intoCart").text(intoCart);
+    }
+    if ((cartTotal == 0) && (intoCart == 0)) {
+        $(".cart").empty().append("<div class=\"offcanvas-body cart\"><img src=\"img/shopping-cart.jpg\" alt=\"Shopping cart\"><div>Votre panier est tristement vide en ce moment</div></div>");
+    } else {
+        $("#totalPrice").text(cartTotal);
+    }
+}
+
+$(".confirmRegister").click(function(){
+    $("#confOrdPagNom").text($("#nom").val());
+    $("#confOrdPagPrenom").text($("#prenom").val());
+    $("#confOrdPagEmail").text($("#email").val());
+    $("#confOrdPagPhone").text($("#phone").val());
+    $("#confOrdPagAdresse").text($("#adresse").val());
+    $("#confOrdPagCP").text($("#cp").val());
+    $("#confOrdPagVille").text($("#ville").val());
+    $("#confOrdPagCart").html("");
+    $(".productInCart").each(function(){
+        $("#confOrdPagCart").append("<div><strong>"+$(".productInCart .prodName").html()+"</strong></div><div id=\"confOrdPagDetails\"><div>Prix unitaire : <strong>"+$(".productInCart .unitPrice").html()+"</strong></div><div>Quantité : <strong>"+$(".productInCart .qty").val()+"</strong></div></div><hr>");
+    })
+    
+    $("#confOrdPagTotal").text($("#totalPrice").html());
+})
